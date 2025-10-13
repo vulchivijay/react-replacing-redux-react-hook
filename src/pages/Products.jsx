e8182890378d2c93222e433b5069824e4369192b
Vijayakumar, Vulchi (External) <Vulchi.Vijayakumar@xerox.com>
2025-10-13 10:03:18 +0530

    Search implementation completed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,7 +5,6 @@ import { useStore } from "../Hooks/useStore";
 export default function Products() {
   const products = useStore(true)[0];
   const sProducts = useStore(true)[4];
-  console.log(JSON.stringify(sProducts, null, 2));
   // if (products.length === 0) {
   //   products = useLoaderData();
   // }
@@ -14,18 +13,13 @@ export default function Products() {
   }
   return (
     <div className="max-w-6xl m-auto sm:px-4">
-      <h1 className="text-xl font-semibold py-4">
-        { sProducts.length > 0 ? 'Search resultL' : 'Products' } :
-        { sProducts.length > 0 ? sProducts.length : products.length }
+      <h1 className="text-sm text-gray-200 font-semibold p2-4">
+        {sProducts.length > 0 ? 'Search result' : 'Products'} :
+        {sProducts.length > 0 ? sProducts.length : products.length}
       </h1>
       <div className="flex flex-wrap m-auto">
-        { sProducts.length > 0 ?
-          sProducts && sProducts.map(product => {
-            <ProductsCard key={product.id} product={product} />
-          }) :
-          products && products.map(product => (
-            <ProductsCard key={product.id} product={product} />
-          ))
+        {sProducts.length > 0 ? sProducts && sProducts.map(product => <ProductsCard key={product.id} product={product} />) :
+          products && products.map(product => <ProductsCard key={product.id} product={product} />)
         }
       </div>
     </div>
